66f9bb75cec49815c6e33b0b2721731c4e0de955
jcpenalo <98penaloza@gmail.com>
2026-02-10 13:13:20 -0800

    better naming and resource reference

--- lib/main.cpp
+++ lib/main.cpp
@@ -11,27 +11,26 @@
 #include <format>
 #include <bits/stdc++.h>
 
-#include <ranges>  // For std::ranges::join_with
+#include <ranges> // For std::ranges::join_with
 #include "Connect4Game.hpp"
 
 using namespace std;
 using namespace views;
 using namespace ranges;
 
-
-int main(int, char**){
+int main(int, char **)
+{
     vector<vector<char>> mat(6, vector<char>(8, 0));
     Connect4Board board = Connect4Board(mat);
     Connect4Player player1 = Connect4Player('x');
     Connect4Player player2 = Connect4Player('o');
     Connect4WinStrategy winStrat = Connect4WinStrategy();
-    vector<Connect4WinStrategy*>  winStrats = {&winStrat};
-
-
+    vector<Connect4WinStrategy *> winStrats = {&winStrat};
 
     Connect4Game game = Connect4Game(&board, &player1, &player2, winStrats);
     game.run();
-    if (game.getWinner()){
+    if (game.getWinner())
+    {
         cout << game.getWinner()->getPlayerMark() << " WINS" << endl;
     }
     return 0;
